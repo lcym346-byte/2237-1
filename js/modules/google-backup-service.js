f693c63661c6b4cffd12c1b2a45e149c3c34f03b
lcym346-byte <lcym346@gmail.com>
2026-05-20 07:01:15 +0800

    Update google-backup-service.js

--- js/modules/google-backup-service.js
+++ js/modules/google-backup-service.js
@@ -12,7 +12,7 @@ let tokenClient = null;
 let accessToken = '';
 let profileEmail = '';
 
-const DEFAULT_CLIENT_ID = '203764995518-sct9k1tpf9fl6nrjh0h7kt8phsr47uas.apps.googleusercontent.com';
+const DEFAULT_CLIENT_ID = '203764995518-sct9k1tpf9fi6nrjh0h7kt8phsr47uas.apps.googleusercontent.com';
 
 function ensureGoogleDriveConfig(){
   if(!state.settings) state.settings = {};
